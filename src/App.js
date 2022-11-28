7e1967ce858bb3422db63462f077dcbbbb96c2ab
snaaz123 <105916806+snaaz123@users.noreply.github.com>
2022-11-28 22:47:59 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h2>Najrin shah</h2>
+    </div>
+  );
+}
+
+export default App;
